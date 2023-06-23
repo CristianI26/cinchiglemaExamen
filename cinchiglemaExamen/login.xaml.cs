0744acf4c6bf9f33b7373f9327699012b5342176
master <master@SMLAB108PC17_23>
2023-06-22 22:00:48 -0500

    Agregar archivos de proyecto.

---
+++ cinchiglemaExamen/login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cinchiglemaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "estudiante2023";
+            string contrasena = "uisrael2023";
+            if (txtUsuario.Text == usuario && txtContrasena.Text == contrasena  )
+            {
+                Navigation.PushAsync(new registro(usuario)); 
+            }
+
+        }
+    }
+}
---
+++ cinchiglemaExamen/registro.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cinchiglemaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro()
+        {
+            InitializeComponent();
+        }
+
+        public registro(string usuario)
+        {
+        }
+
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
